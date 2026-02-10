8f5ea6298eda99fc17ee9b85042f21bd997efd10
aref <devfera14@gmail.com>
2026-02-10 14:07:09 -0300

    alteracao basica na funcao

--- lib/utils/funcoes.dart
+++ lib/utils/funcoes.dart
@@ -5,3 +5,16 @@ void main() {
 void saudacao() {
   print("ola, seja muito bem vindo");
 }
+
+void mostrarNome(String nome) {
+  print("Seu nome e $nome");
+}
+
+void main2() {
+  int resultado = somar(10, 5);
+  print(resultado);
+}
+
+int somar(int a, int b) {
+  return a + b;
+}
