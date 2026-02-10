42c2024ca619761b3748554ace50c8acd4a01dee
aref <devfera14@gmail.com>
2026-02-10 14:27:43 -0300

    alteracoes

---
+++ lib/colecoes.dart
@@ -0,0 +1,29 @@
+void main() {
+  List<String> frutas = ["banana", "maca", "pera"];
+  print(frutas);
+}
+
+print(frutas[0]); // banana
+print (frutas [1]); // maca
+print (frutas [2]); // pera
+
+
+ futas.add("jabuticaba");
+ frutas.remove("banana");
+ print(frutas);
+
+
+
+
+for (int i = 0; i < nomes.length; i++) {
+  print(nomes[i]);
+}
+
+
+Map<String, dynamic> pessoa = {
+  "nome": "Aref",
+  "idade": 25,
+  "altura": 1.75
+};
+
+print(pessoa["nome"]);
